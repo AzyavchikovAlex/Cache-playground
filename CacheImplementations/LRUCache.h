2cd800131a02d5ae5369ab4e7766b34efd7dec7d
AlexAzyavchikov <azyavchikov.alex@gmail.com>
2026-07-11 07:30:22 +0300

    LRU refactoring

--- CacheImplementations/LRUCache.h
+++ CacheImplementations/LRUCache.h
@@ -14,9 +14,9 @@ class LRUCache : public Cache<K> {
  private:
   std::unordered_map<K, typename std::list<K>::iterator> keys_positions_;
   std::list<K> sorted_keys_;
-  size_t size_;
+  size_t max_size_{0};
 
-  void EraseInternal(typename std::unordered_map<K, typename std::list<K>::iterator>::iterator it) {
+  void EraseInternal(auto it) {
     if (it == keys_positions_.end()) {
       throw std::runtime_error{"Invalid iterator in erase function"};
     }
@@ -24,22 +24,23 @@ class LRUCache : public Cache<K> {
     keys_positions_.erase(it);
   }
 
-  void TouchKeyInternal(typename std::unordered_map<K, typename std::list<K>::iterator>::iterator it) {
+  void TouchKeyInternal(auto it) {
     auto list_it = it->second;
     sorted_keys_.splice(sorted_keys_.begin(), sorted_keys_, list_it); // move to front
   }
 
  public:
-  explicit LRUCache(size_t size) : size_(size) {
+  explicit LRUCache(size_t size) : max_size_(size) {
     if (size == 0) {
       throw std::runtime_error{"Zero size for LRU cache is invalid"};
     }
   }
 
-  ~LRUCache() override = default;
-
   K EvictKey() {
-    auto key_to_erase = *sorted_keys_.rbegin();
+    if (sorted_keys_.empty()) {
+      throw std::runtime_error{"Cannot evict key from empty cache"};
+    }
+    K key_to_erase = *sorted_keys_.rbegin();
     EraseInternal(keys_positions_.find(key_to_erase));
     return key_to_erase;
   }
@@ -55,7 +56,7 @@ class LRUCache : public Cache<K> {
     while (sorted_keys_.size() > new_size) {
       EvictKey();
     }
-    size_ = new_size;
+    max_size_ = new_size;
   }
 
   void Insert(const K& key) override {
@@ -63,8 +64,9 @@ class LRUCache : public Cache<K> {
       TouchKeyInternal(it);
       return;
     }
-
-    if (sorted_keys_.size() >= size_) {
+    
+    assert(sorted_keys_.size() <= max_size_);
+    if (sorted_keys_.size() == max_size_) {
       EvictKey();
     }
 
@@ -76,8 +78,9 @@ class LRUCache : public Cache<K> {
     if (auto it = keys_positions_.find(key); it != keys_positions_.end()) {
       EraseInternal(it);
       return true;
+    } else {
+      return false;
     }
-    return false;
   }
 
   void Touch(const K& key) override {
